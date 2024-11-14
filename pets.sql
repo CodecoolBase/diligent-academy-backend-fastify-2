0bb4c05b5ee133315550dc92ce14db5ac650575a
Daniel Hatas <21317298+samson84@users.noreply.github.com>
2024-11-14 14:57:28 +0100

    Add multiple endpoints, integrate owners.

--- pets.sql
+++ pets.sql
@@ -1,8 +1,5 @@
 DROP TABLE IF EXISTS pet;
-
 DROP TABLE IF EXISTS pet_owner;
-
-DROP TABLE IF EXISTS pet_kind;
 
 CREATE TABLE pet_owner (
 	id serial PRIMARY KEY,
@@ -10,7 +7,7 @@ CREATE TABLE pet_owner (
 	age integer
 );
 
-CREATE TABLE pet_kind (id serial PRIMARY KEY, name varchar(20));
+CREATE TYPE pet_kind as ENUM('dog', 'cat', 'reptile', 'insect');
 
 CREATE TABLE pet (
 	id serial PRIMARY KEY,
@@ -18,9 +15,8 @@ CREATE TABLE pet (
 	age integer,
 	weight_in_kg numeric,
 	owner_id integer,
-	kind_id integer,
-	FOREIGN key (owner_id) REFERENCES pet_owner (id),
-	FOREIGN key (kind_id) REFERENCES pet_kind (id)
+	kind pet_kind,
+	FOREIGN key (owner_id) REFERENCES pet_owner (id)
 );
 
 -- Insert pet owners
@@ -38,52 +34,42 @@ VALUES
 	('Ian', 55),
 	('Julia', 38);
 
--- Insert pet kinds
 INSERT INTO
-	pet_kind (name)
+	pet (name, age, weight_in_kg, owner_id, kind)
 VALUES
-	('dog'),
-	('cat'),
-	('reptile'),
-	('insect');
-
-INSERT INTO
-	pet (name, age, weight_in_kg, owner_id, kind_id)
-VALUES
-	('Bella', 3, 12.5, 1, 1),
+	('Bella', 3, 12.5, 1, 'dog'),
 	-- Dog owned by Alice
-	('Max', 5, 15.0, 1, 1),
+	('Max', 5, 15.0, 1, 'dog'),
 	-- Dog owned by Alice
-	('Whiskers', 2, 4.5, 2, 2),
+	('Whiskers', 2, 4.5, 2, 'cat'),
 	-- Cat owned by Bob
-	('Shadow', 4, 4.8, 2, 2),
+	('Shadow', 4, 4.8, 2, 'cat'),
 	-- Another Cat owned by Bob
-	('Fluffy', 6, 0.05, NULL, 4),
+	('Fluffy', 6, 0.05, NULL, 'insect'),
 	-- Insect without an owner
-	('Rex', 8, 20.0, 5, 1),
+	('Rex', 8, 20.0, 5, 'dog'),
 	-- Dog owned by Emma
-	('Goldie', 3, 0.2, NULL, 3),
+	('Goldie', 3, 0.2, NULL, 'reptile'),
 	-- Reptile without an owner
-	('Sasha', 4, 8.3, 6, 2),
+	('Sasha', 4, 8.3, 6, 'cat'),
 	-- Cat owned by Frank
-	('Buddy', 2, 10.0, 1, 1),
+	('Buddy', 2, 10.0, 1, 'dog'),
 	-- Another Dog owned by Alice
-	('Cleo', 5, 12.5, NULL, 2),
+	('Cleo', 5, 12.5, NULL, 'cat'),
 	-- Cat without an owner
-	('Ziggy', 3, 2.1, 7, 3),
+	('Ziggy', 3, 2.1, 7, 'reptile'),
 	-- Reptile owned by Grace
-	('Tiny', 1, 0.02, NULL, 4),
+	('Tiny', 1, 0.02, NULL, 'insect'),
 	-- Insect without an owner
-	('Milo', 4, 11.2, 8, 1),
+	('Milo', 4, 11.2, 8, 'dog'),
 	-- Dog owned by Hannah
-	('Bella', 5, 13.0, 8, 1),
+	('Bella', 5, 13.0, 8, 'dog'),
 	-- Another Dog owned by Hannah
-	('Simba', 2, 4.5, 9, 2),
+	('Simba', 2, 4.5, 9, 'cat'),
 	-- Cat owned by Ian
-	('Toto', 6, 7.3, NULL, 1),
+	('Toto', 6, 7.3, NULL, 'dog'),
 	-- Dog without an owner
-	('Slinky', 3, 1.8, 10, 3),
+	('Slinky', 3, 1.8, 10, 'reptile'),
 	-- Reptile owned by Julia
-	('Buzz', 2, 0.03, 6, 4);
-
--- Insect owned by Frank
+	('Buzz', 2, 0.03, 6, 'insect');
+	-- Insect owned by Frank
--- src/controller/app.ts
+++ src/controller/app.ts
@@ -2,7 +2,11 @@ import fastify from 'fastify';
 import { PetService } from '../service/pet.service';
 import { PetRepository } from '../repository/pet.repository';
 import { DbClient } from '../db';
-import { PetToCreate } from '../entity/pet.type';
+import { JsonSchemaToTsProvider } from '@fastify/type-provider-json-schema-to-ts'
+import { getPetByIdSchema, getPetsSchema, postPetsSchema, putPetsToOwnersSchema } from './pet.schemas';
+import { OwnerRepository } from '../repository/owner.repository';
+import { OwnerService } from '../service/owner.service';
+import { getOwnerByIdSchema, getOwnersSchema, postOwnerSchema } from './owner.schemas';
 
 type Dependencies = {
   dbClient: DbClient;
@@ -13,25 +17,78 @@ export default function createApp(options = {}, dependencies: Dependencies) {
 
   const petRepository = new PetRepository(dbClient);
   const petService = new PetService(petRepository);
-  
+  const ownerRepository = new OwnerRepository(dbClient);
+  const ownerService = new OwnerService(ownerRepository);
 
   const app = fastify(options)
+    .withTypeProvider<JsonSchemaToTsProvider>()
 
-  app.get('/api/pets', async () => {
-    const pets = await petService.getAll();
-    return pets;
-  })
+  app.get(
+    '/api/pets',
+    { schema: getPetsSchema },
+    async () => {
+      const pets = await petService.getAll();
+      return pets;
+    })
 
-  type PostPetsRoute = {
-    Body: PetToCreate;
-  }
-  app.post<PostPetsRoute>('/api/pets', async (request, reply) => {
-    const { body: petToCreate } = request;
+  app.get(
+    '/api/pets/:id',
+    { schema: getPetByIdSchema },
+    async (request) => {
+      const { id } = request.params;
+      const pets = await petService.getById(id);
+      return pets;
+    })
 
-    const created = await petService.create(petToCreate);
-    reply.status(201);
-    return created;
-  })
+
+  app.post(
+    '/api/pets',
+    { schema: postPetsSchema },
+    async (request, reply) => {
+      const { body: petToCreate } = request;
+
+      const created = await petService.create(petToCreate);
+      reply.status(201);
+      return created;
+    })
+
+  app.put(
+    '/api/owners/:ownerId/pets/:petId',
+    { schema: putPetsToOwnersSchema },
+    async (request) => {
+      const { petId, ownerId } = request.params;
+      const updated = await petService.adopt(petId, ownerId);
+      return updated;
+    }
+  )
+
+  app.get(
+    '/api/owners',
+    { schema: getOwnersSchema },
+    async () => {
+      return await ownerService.getAll();
+    }
+  )
+
+  app.get(
+    '/api/owners/:id',
+    { schema: getOwnerByIdSchema },
+    async (request) => {
+      const { id } = request.params;
+      return await ownerService.getById(id);
+    }
+  )
+
+  app.post(
+    '/api/owners',
+    { schema: postOwnerSchema },
+    async (request, reply) => {
+      const ownerProps = request.body;
+      const created = await ownerService.create(ownerProps);
+      reply.status(201);
+      return created;
+    }
+  )
 
   return app;
 }
